5aa23c78fcf04d4620a8efd25dba71b273bd128b
yzq <504767693@qq.com>
2021-04-29 16:49:57 +0800

    修改上传文件格式

--- src/uploadFile/index.js
+++ src/uploadFile/index.js
@@ -1,14 +1,13 @@
 /**
  * @file: 上传文件
  * @author: yan zhenqing
- * @param {file} e 上传的文件
+ * @param {file} 上传的文件
  * @param ['txt', 'docx', 'png'...] typeList 文件后缀名，字符串（小写）数组类型
  */
 
-const uploadFile = (e, typeList) => {
+const uploadFile = (file, typeList) => {
   return new Promise((resolve, reject) => {
-    if (e) {
-      const file = e.target.files[0]
+    if (file) {
       const filetype = file.name.substring(file.name.lastIndexOf('.') + 1).toLowerCase()
       if (Array.isArray(typeList)) {
         typeList.map(type => type.toLowerCase()).includes(filetype)
